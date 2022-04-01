6f489d496fbda475efcb7678b3d9e7ca122b7dc1
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-04-02 00:42:14 +0530

    Dron'es propeller controller in C

---
+++ Bit Manipulation/bitmasks/main.c
@@ -0,0 +1,110 @@
+// used to set/unset or check bit state
+#include <stdio.h>
+#define OFF 0
+#define ON 1
+int main(void)
+{
+    int DRONES_Propeller_STATE = OFF;
+
+    // enable 1rst head left Propeller
+    int mask = 0b0000000000000001;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 1rst head righ Propeller
+    mask = 0b0000000000000010;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 2rst left Propeller
+    mask = 0b0000000000000100;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 2rst right Propeller
+    mask = 0b0000000000001000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 3rd left Propeller
+    mask = 0b0000000000010000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 3rd right Propeller
+    mask = 0b000000000010000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 4rth left Propeller
+    mask = 0b0000000000100000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 4rth right Propeller
+    mask = 0b000000001000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 5th left Propeller
+    mask = 0b0000000001000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 5th right Propeller
+    mask = 0b000000100000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 6th left Propeller
+    mask = 0b0000000010000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 6th right Propeller
+    mask = 0b000010000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 7th left Propeller
+    mask = 0b0000000100000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 7th right Propeller
+    mask = 0b000000100000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 8th left Propeller
+    mask = 0b0000001000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 8th right Propeller
+    mask = 0b000010000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 9th left Propeller
+    mask = 0b0000010000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 9th right Propeller
+    mask = 0b001000000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 10th left Propeller
+    mask = 0b0010000000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    // enable 10th right Propeller
+    mask = 0b010000000000000;
+    DRONES_Propeller_STATE = DRONES_Propeller_STATE | mask;
+    printf("\nDRONE STATE : %d, \n", DRONES_Propeller_STATE);
+
+    return 0;
+}
--- Bit Manipulation/left shift/loop.c
+++ Bit Manipulation/left shift/loop.c
@@ -23,16 +23,18 @@ unsigned long long int Dec2Bin(unsigned long int dec)
 
 int main(void)
 {
-    unsigned  long long int w1 = 0;
-    unsigned long long int mask = w1 << (sizeof(w1) * 8);
-    printf("sizeof long long int : %zd\n",sizeof(unsigned long long int));
+    unsigned  long long int w1 = 0;//0000
+    unsigned long long int mask = w1 << (sizeof(w1) * 8)-1;//1000
+    printf("sizeof long long int : %zd\nand the mask is : %lld\n",sizeof(unsigned long long int),mask);
 
     while (w1 ^ mask)
     {
         static int counter = 1;
-        printf("loop round : %d\n", counter);
+        
+        printf("loop round : %d\nvalue of w1 : %lld\n", counter,w1);
         ++counter;
         w1 <<= 1; // or w1= w1<<1;
+
     };
     printf("\n[System Message] : Program Successfulyy Executed!\n");
 
