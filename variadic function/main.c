7254ee5bb8b7acde51c932c67b01b847f821b25b
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-04-12 22:06:29 +0530

    variadic function done!

--- variadic function/main.c
+++ variadic function/main.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdarg.h>
-double add(const double d1,const double d2, ...)
+double add(unsigned short int count, ...)
 {
     va_list arg;
-    va_start(arg, d2);
-    double Sum = d1+d2, values = 0.0;
-    while ((values = va_arg(arg, double)))
+    va_start(arg, count);
+    double sum = 0;
+    for (int i = 0; i < count; ++i)
     {
-        Sum += values;
-        printf("..");
+        double n = va_arg(arg, double);
+        sum += n;
+    };
+
+    va_end(arg);
+    return sum;
+};
+
+void print(int count, ...)
+{
+    va_list arg;
+    va_start(arg, count);
+    for (int i = 0; i < count; ++i)
+    {
+        const char *str = va_arg(arg, const char *);
+        printf("%s", str);
     };
     va_end(arg);
-    return Sum;
 };
 
 int main(void)
 {
-   double sum=add(3.5,3.5,3.5);
-   printf("%lf\n",sum);
-
+    double sum = add(3, 3.5, 3.5, 35.5);
+    printf("%lf\n", sum);
+    print(1, "\nPrinting via variadic function print ....\n");
+    print(5, "Hello, World\n", "Hello, Earth\n", "Hello, Vishal\n", "Hello, vengine\n", "Hello, Galaxy\n");
+    print(1, "\nshutting down ...\n");
 
     return 0;
 }
